89dd3059dbc287556c2b0aa1c5969527261b047b
Juliette Hoppenot <juliette.hoppenot@gmail.com>
2019-11-13 18:28:42 +0100

    exo deux

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pitaya = Restaurant.create(name: "Pitaya", address: "rue Parmentier", phone_number: "0123456789", category: "chinese")
+mcdo = Restaurant.create(name: "Mcdo", address: "rue Cracra", phone_number: "0123456787", category: "chinese")
+tintin = Restaurant.create(name: "Tintin", address: "rue de Milou", phone_number: "0123456765", category: "japanese")
+bocamexa = Restaurant.create(name: "Bocamexa", address: "rue du Tacos", phone_number: "0673456765", category: "belgian")
+superresto = Restaurant.create(name: "Superresto", address: "rue de la gloire", phone_number: "0123451234", category: "french")
